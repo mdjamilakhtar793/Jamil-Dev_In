8774cfd446a759ac1c502aee3f02eb1bc704d10b
MD JAMIL AKHTAR <131196293+mdjamilakhtar793@users.noreply.github.com>
2024-11-03 14:49:12 +0530

    Some Changes

--- components/BentoGridDemo.tsx
+++ components/BentoGridDemo.tsx
@@ -14,18 +14,21 @@ import { BentoGrid, BentoGridItem } from "./ui/bento-grid";
 
 export function BentoGridThirdDemo() {
   return (
-    <BentoGrid className="max-w-4xl mx-auto md:auto-rows-[20rem]">
-      {items.map((item, i) => (
-        <BentoGridItem
-          key={i}
-          title={item.title}
-          description={item.description}
-          header={item.header}
-          className={cn("[&>p:text-lg]", item.className)}
-          icon={item.icon}
-        />
-      ))}
-    </BentoGrid>
+    <div className="">
+      <h1 className="heading">About me with code 👨‍⚖️</h1>
+      <BentoGrid className="max-w-4xl mx-auto md:auto-rows-[20rem]">
+        {items.map((item, i) => (
+          <BentoGridItem
+            key={i}
+            title={item.title}
+            description={item.description}
+            header={item.header}
+            className={cn("[&>p:text-lg]", item.className)}
+            icon={item.icon}
+          />
+        ))}
+      </BentoGrid>
+    </div>
   );
 }
 
